fdb20724c81081281673b517d3a89a8fd19f9729
CarinaBaeck <cabaeck@edu.aau.at>
2023-03-05 19:50:53 +0100

    Added several streams:
    -Socket creates connection to server
    -ByteArrayOutputStream writes data into a byte array
    -ByteArrayOutputStream.writeTo(outputStream) sends byte array to the server
    -InputStream receives the response from the server
    -BufferedReader uses one line for response
    -.readLine() saves answer as a String
    -Added runOnUiThread for changing the interface when there is server communication

--- app/src/main/java/com/example/networktesting/MainActivity.kt
+++ app/src/main/java/com/example/networktesting/MainActivity.kt
@@ -4,12 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import java.io.BufferedReader
+import java.io.ByteArrayOutputStream
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.Socket
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var matrNr: EditText //lateinit: value does not need to be assigned yet
     lateinit var button: Button
     lateinit var response: EditText
+    lateinit var textView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +24,7 @@ class MainActivity : AppCompatActivity() {
 
         matrNr = findViewById(R.id.matrNr)
         response = findViewById(R.id.response)
+        textView = findViewById(R.id.textView)
         button = findViewById(R.id.button)
 
         button.setOnClickListener(){
@@ -25,8 +33,25 @@ class MainActivity : AppCompatActivity() {
     }
     private fun sendToServer(matrNr: String){
         Thread(Runnable{
+            val socket = Socket("se2-isys.aau.at",53212)
+            val outputStream = socket.getOutputStream()
 
+            //write matrNr to byte array and send to server
+            val ByteArrayOutputStream = ByteArrayOutputStream()
+            ByteArrayOutputStream.write(matrNr.toByteArray())
+            ByteArrayOutputStream.writeTo(outputStream)
 
-        })
+            //read response from server
+            val inputStream = socket.getInputStream()
+            val bufferedReader = BufferedReader(InputStreamReader(inputStream))
+            val response = bufferedReader.readLine()
+
+            //update UI with response
+            runOnUiThread{
+                textView.setText(response)
+            }
+
+            socket.close()
+        }).start()
     }
 }
