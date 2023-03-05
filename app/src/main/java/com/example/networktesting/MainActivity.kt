136caf0fb875dd1b2ea119636732b5eb50694b7f
CarinaBaeck <cabaeck@edu.aau.at>
2023-03-05 19:23:47 +0100

    used findViewById for variables
    button.setOnClickListener for using method sendToServer

--- app/src/main/java/com/example/networktesting/MainActivity.kt
+++ app/src/main/java/com/example/networktesting/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.networktesting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var matrNr: EditText //lateinit: value does not need to be assigned yet
+    lateinit var button: Button
+    lateinit var response: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        matrNr = findViewById(R.id.matrNr)
+        response = findViewById(R.id.response)
+        button = findViewById(R.id.button)
+
+        button.setOnClickListener(){
+            sendToServer(matrNr.toString())
+        }
+    }
+    private fun sendToServer(matrNr: String){
+        Thread(Runnable{
+
+
+        })
     }
 }
