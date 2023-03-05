973cdf01731edc200d5d811ca6499a9dd0a34f82
CarinaBaeck <cabaeck@edu.aau.at>
2023-03-05 20:06:31 +0100

    Changes due to an error:
    -reponseField is an EditText element instead of a TextView so that the text can be modified by the server's response
    -uses-permission added to AndroidManifest.xml for Internet & Access Network State
    -try and catch added for testing an error

--- app/src/main/java/com/example/networktesting/MainActivity.kt
+++ app/src/main/java/com/example/networktesting/MainActivity.kt
@@ -15,16 +15,14 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var matrNr: EditText //lateinit: value does not need to be assigned yet
     lateinit var button: Button
-    lateinit var response: EditText
-    lateinit var textView: TextView
+    lateinit var responseField: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         matrNr = findViewById(R.id.matrNr)
-        response = findViewById(R.id.response)
-        textView = findViewById(R.id.textView)
+        responseField = findViewById(R.id.responseField)
         button = findViewById(R.id.button)
 
         button.setOnClickListener(){
@@ -33,25 +31,30 @@ class MainActivity : AppCompatActivity() {
     }
     private fun sendToServer(matrNr: String){
         Thread(Runnable{
-            val socket = Socket("se2-isys.aau.at",53212)
-            val outputStream = socket.getOutputStream()
+            try {
+                val socket = Socket("se2-isys.aau.at", 53212)
+                val outputStream = socket.getOutputStream()
 
-            //write matrNr to byte array and send to server
-            val ByteArrayOutputStream = ByteArrayOutputStream()
-            ByteArrayOutputStream.write(matrNr.toByteArray())
-            ByteArrayOutputStream.writeTo(outputStream)
+                //write matrNr to byte array and send to server
+                val ByteArrayOutputStream = ByteArrayOutputStream()
+                ByteArrayOutputStream.write(matrNr.toByteArray())
+                ByteArrayOutputStream.writeTo(outputStream)
 
-            //read response from server
-            val inputStream = socket.getInputStream()
-            val bufferedReader = BufferedReader(InputStreamReader(inputStream))
-            val response = bufferedReader.readLine()
+                //read response from server
+                val inputStream = socket.getInputStream()
+                val bufferedReader = BufferedReader(InputStreamReader(inputStream))
+                val response = bufferedReader.readLine()
 
-            //update UI with response
-            runOnUiThread{
-                textView.setText(response)
+                //update UI with response
+                runOnUiThread {
+                    responseField.setText(response)
+                }
+
+                socket.close()
+            } catch (e: Exception){
+                "Error occurred"
             }
 
-            socket.close()
         }).start()
     }
 }
